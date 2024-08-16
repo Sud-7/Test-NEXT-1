f452cb4c28b9501afdb4aa48dbe5759d3e40ed4a
Sud <155895839+Sud-7@users.noreply.github.com>
2024-08-16 14:04:06 +0530

    for linking and navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,21 @@
 "use client";
+import Link from "next/link";
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <main className={styles.main}>
       <div>
-        This is Filty Frank<br></br>
-        <button onClick={() => alert("Hey boi")}>Click me</button>
+        <h1>HOME PAAGE</h1>
+        <br></br>
+        <Link href="/login">Go to about Login</Link>
+        <br></br>
+        <Link href="/about">Go to about Page</Link>
+        <br></br>
+        <button onClick={() => router.push("/login")}>GO TO LOGIN</button>
+        <button onClick={() => router.push("/about")}>GO TO ABOUT</button>
       </div>
     </main>
   );

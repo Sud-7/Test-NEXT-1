a5e82ed8602b66ab479fd6219fc6f9777c159047
Sud <155895839+Sud-7@users.noreply.github.com>
2024-08-17 11:18:40 +0530

    Added changes

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import "./login.css";
+
 export default function Layout({ children }) {
   return (
     <div>
       <h1>Common Layout for Login Page</h1>
+      <ul className="login-menu">
+        <li>
+          <h4>Login NavBar</h4>
+        </li>
+        <li>
+          <Link href="/login">Go to Login PAGE</Link>
+        </li>
+        <li>
+          <Link href="/login/facultylogin">Go to Faculty Login</Link>
+        </li>
+        <li>
+          <Link href="/login/studentlogin">Go to Student Login</Link>
+        </li>
+      </ul>
       {children}
     </div>
   );

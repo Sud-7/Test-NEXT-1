a551f271951916e139579b78886b02c1f689282d
Sud <155895839+Sud-7@users.noreply.github.com>
2024-08-17 13:46:06 +0530

    final commit for v14

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <div>
+      <h1>This Page is Not available right NOW.</h1>
+      <p>Hang on Till next life!!</p>
+    </div>
+  );
+};
+
+export default Notfound;
